5fba7e2fb8d586b2c6bc431854c9c0eaf2368b4a
anamaria <anamariacampos20@hotmail.com>
2025-10-22 17:51:52 -0600

    Change to wordpress site

--- administrator/logs/joomla_core_errors.php
+++
@@ -1,7 +0,0 @@
-#
-#<?php die('Forbidden.'); ?>
-#Date: 2025-10-15 00:48:29 UTC
-#Software: Joomla! 5.4.0 Stable [ Kutegemea ] 14-October-2025 16:00 GMT
-
-#Fields: datetime	priority clientip	category	message
-2025-10-15T00:48:29+00:00	ERROR ::1	system	The template for this display is not available.
